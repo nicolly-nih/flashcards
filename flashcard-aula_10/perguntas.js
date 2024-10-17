ebe1290fb6e0a506689d6e22b15f7785b3ceea0b
Nicolly <142987397+nicolly-nih@users.noreply.github.com>
2024-10-17 07:12:13 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciẽncias',
+    'O que é uma galáxia?',
+    'É um vasto sistema de estrelas, gás, poeira e matéria escura que são mantidos juntos pela gravidade.'
+)
+
+criaCartao(
+    'Ciências',
+    'O que diz a teoria do Big Bang?',
+    'A teoria do Big Bang é a principal explicação científica sobre a origem do universo. Ela propõe que o universo começou como um ponto quente e denso a cerca de 13,8 bilhões de anos.'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é um buraco negro?',
+    'É uma região do espaço onde a gravidade é tão forte que nada, nem mesmo a luz, pode escapar dele.Ele se forma quando estrelas massivas colapsam sob sua própria gravidade no final de suas vidas.'
+)
+
+criaCartao(
+    'Ciências',
+    'O que constitui a matéria escura?',
+    'A matéria escura é uma forma de matéria que não emite, absorve ou reflete luz, tornando-se invisível e detectável apenas por seus efeitos gravitacionais em objetos visíveis. Cerca de 27% do unuverso é constituído de matéria escura.'
+)
